7a30ead1b0a32d93039a08bfff2ee9f0173c8b1b
yshkd <yushi.public@gmail.com>
2022-10-02 00:31:39 +0900

    Add vue particles

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,40 @@
 <template>
-  <h1 class="pl-24 pt-12 text-3xl text-red-600 underline">
-    Hello world!
-  </h1>
+  <div id="app">
+    <div class="text h-screen w-screen flex justify-center items-center">
+      <p class="text-4xl font-mono font-black">gm,0ch</p>
+    </div>
+    <vue-particles
+      color="#000000"
+      :particleOpacity="0.7"
+      :particlesNumber="80"
+      shapeType="circle"
+      :particleSize="4"
+      linesColor="#000000"
+      :linesWidth="1"
+      :lineLinked="true"
+      :lineOpacity="0.4"
+      :linesDistance="150"
+      :moveSpeed="5"
+      :hoverEffect="true"
+      hoverMode="grab"
+      :clickEffect="true"
+      clickMode="push"
+      class="h-screen fixed inset-0"
+    >
+    </vue-particles>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
+  name: 'IndexPage',
+  components: {
+  },
 }
 </script>
+
+<style scoped>
+#app{
+  background-color: white;
+}
+</style>
